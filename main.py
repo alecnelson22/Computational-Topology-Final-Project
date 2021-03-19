852c4b74bbc2be4867962c2d29d102b3b681cc5b
Devin Lange <devinscottlange@gmail.com>
2021-03-19 17:52:24 -0600

    add example usage, add very simple test data, cleanup

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import pandas as pd
-# import numpy as np
-# import csv
-# import os
+import matplotlib.pyplot as plt
 
 DATA_FOLDER = './data/'
 
-
-
 def main():
-    # Load file
+    # Load file and build data structure
     folder = 'Pedestrian Dynamics Data Archive/bottleneck/'
     filename = '250_q_45_h0.csv'
-    data = buildDataStructures(folder, filename)
+    data = build_data_structures(folder, filename)
     trajectories, frames, times, col_keys = data
-    printBasicStats(data)
 
+    # do analysis with data structure
+    print_basic_stats(data) 
+    plot_trajectories(data, True)
     return
 
-def printBasicStats(data):
+def print_basic_stats(data):
     trajectories, frames, times, col_keys = data
     trajectory_ids = trajectories.groups.keys()
     number_of_trajectories = len(trajectory_ids)
@@ -26,12 +24,18 @@ def printBasicStats(data):
     print('# frames: {}'.format(number_of_frames))
     return
 
-def basicPlot(data):
+def plot_trajectories(data, include_center_of_mass):
     trajectories, frames, times, col_keys = data
-
+    (t_key, x_key, y_key, id_key) = col_keys
+    fig, ax = plt.subplots()
+    trajectories.plot(ax = ax, x = x_key, y = y_key, legend = False)
+    if include_center_of_mass:
+        center_of_mass_list = frames.agg('mean')
+        center_of_mass_list.plot(ax = ax, x = x_key, y = y_key, linewidth=3, color='black',legend = False)
+    plt.show()
     return
 
-def buildDataStructures(folder, filename):
+def build_data_structures(folder, filename, verbose = False):
     '''
     Returns tuple with 4 values:
         trajectories - Pandas GroupBy object where each group is a trajectory
@@ -40,29 +44,35 @@ def buildDataStructures(folder, filename):
         col_keys - tuple with 4 relevant column key strings (time, x, y, id)
     '''
     data_frame = pd.read_csv(DATA_FOLDER + folder + filename)
+    if verbose:
+        print(data_frame.describe())
 
     # Sanity check columns, get variants of keys
-    t_key = checkColumn(['time', 't', 'T', 'Time'], data_frame)
-    x_key = checkColumn(['x', 'X'], data_frame)
-    y_key = checkColumn(['y', 'Y'], data_frame)
-    id_key = checkColumn(['id', 'ID'], data_frame)
-
+    t_key = check_column(['time', 't', 'T', 'Time'], data_frame)
+    x_key = check_column(['x', 'X'], data_frame)
+    y_key = check_column(['y', 'Y'], data_frame)
+    id_key = check_column(['id', 'ID'], data_frame)
     col_keys = (t_key, x_key, y_key, id_key)
 
+    # Filter to only the relevant 4 columns to remove junk and make any later calculations faster
+    data_frame = data_frame[list(col_keys)]
+
     # group points into trajectories based on trajectory id
-    trajectories = data_frame.groupby(id_key)
+    trajectories = data_frame.copy().groupby(id_key)
     # sort trajectories based on time
-    for id, trajectory in trajectories:
+    for _id, trajectory in trajectories:
         trajectory.sort_values(t_key)
 
     # group data by time
     frames = data_frame.groupby(t_key)
+    if verbose:
+        print(frames.describe())
 
     times = list(frames.groups.keys())
     times.sort()
     return trajectories, frames, times, col_keys
 
-def checkColumn(key_options, data_frame):
+def check_column(key_options, data_frame):
     for key in key_options:
         if key in data_frame.columns:
             return key
