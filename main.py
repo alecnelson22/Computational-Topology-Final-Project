52c42446c39ef5ef1ae29e9484bbd63650841095
Devin Lange <devinscottlange@gmail.com>
2021-03-19 16:48:04 -0600

    add data structure building

--- main.py
+++ main.py
@@ -5,24 +5,45 @@ import pandas as pd
 
 DATA_FOLDER = './data/'
 
-# Load csv data to dict (csv formatted)
-# fd = 'data/data/Pedestrian Dynamics Data Archive/hallway/'
-# fn = 'BI_CORR_400_A_1.csv'
-
-# data = []
-# with open(os.path.join(fd, fn), 'r') as file:
-#     for i,line in enumerate(csv.reader(file)):
-#         if i == 0:
-#             attributes = line
-#         else:
-#             entry = {}
-#             for j,item in enumerate(line):
-#                 entry[attributes[j]] = item
-#             data.append(entry)
-
-# print('Data loaded')
 
 
-data_frame = pd.read_csv(DATA_FOLDER + 'Pedestrian Dynamics Data Archive/hallway/' + 'BI_CORR_400_A_1.csv')
+def main():
+    # Load file
+    folder = 'Pedestrian Dynamics Data Archive/bottleneck/'
+    filename = '250_q_45_h0.csv'
+    trajectories, frames, times = buildDataStructures(folder, filename)
+    
 
-print(data_frame.describe())
+    return
+
+def buildDataStructures(folder, filename):
+    data_frame = pd.read_csv(DATA_FOLDER + folder + filename)
+
+    # Sanity check columns, get variants of keys
+    tKey = checkColumn(['time', 't', 'T', 'Time'], data_frame)
+    xKey = checkColumn(['x', 'X'], data_frame)
+    yKey = checkColumn(['y', 'Y'], data_frame)
+    idKey = checkColumn(['id', 'ID'], data_frame)
+
+    # group points into trajectories based on trajectory id
+    trajectories = data_frame.groupby(idKey)
+    # sort trajectories based on time
+    for id, trajectory in trajectories:
+        trajectory.sort_values(tKey)
+
+    # group data by time
+    frames = data_frame.groupby(tKey)
+
+    times = frames.groups.keys()
+    times.sort()
+    return trajectories, frames, times
+
+def checkColumn(keyOptions, data_frame):
+    for key in keyOptions:
+        if key in data_frame.columns:
+            return key
+    # print('Failed to find required key (' + keyOptions.join(',') + ')')
+    quit('Failed to find required key (' + ','.join(keyOptions) + ')')
+
+if __name__ == '__main__':
+    main()
