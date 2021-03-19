3474695f8a827425c2608d400f9cb8b13d212db7
Devin Lange <devinscottlange@gmail.com>
2021-03-19 16:53:36 -0600

    fix bug, add simple example

--- main.py
+++ main.py
@@ -11,39 +11,48 @@ def main():
     # Load file
     folder = 'Pedestrian Dynamics Data Archive/bottleneck/'
     filename = '250_q_45_h0.csv'
-    trajectories, frames, times = buildDataStructures(folder, filename)
-    
+    data = buildDataStructures(folder, filename)
+    trajectories, frames, times = data
+    printBasicStats(data)
 
+    return
+
+def printBasicStats(data):
+    trajectories, frames, times = data
+    trajectory_ids = trajectories.groups.keys()
+    number_of_trajectories = len(trajectory_ids)
+    number_of_frames = len(times)
+    print('# trajectories: {}'.format(number_of_trajectories))
+    print('# frames: {}'.format(number_of_frames))
     return
 
 def buildDataStructures(folder, filename):
     data_frame = pd.read_csv(DATA_FOLDER + folder + filename)
 
     # Sanity check columns, get variants of keys
-    tKey = checkColumn(['time', 't', 'T', 'Time'], data_frame)
-    xKey = checkColumn(['x', 'X'], data_frame)
-    yKey = checkColumn(['y', 'Y'], data_frame)
-    idKey = checkColumn(['id', 'ID'], data_frame)
+    t_key = checkColumn(['time', 't', 'T', 'Time'], data_frame)
+    x_key = checkColumn(['x', 'X'], data_frame)
+    y_key = checkColumn(['y', 'Y'], data_frame)
+    id_key = checkColumn(['id', 'ID'], data_frame)
 
     # group points into trajectories based on trajectory id
-    trajectories = data_frame.groupby(idKey)
+    trajectories = data_frame.groupby(id_key)
     # sort trajectories based on time
     for id, trajectory in trajectories:
-        trajectory.sort_values(tKey)
+        trajectory.sort_values(t_key)
 
     # group data by time
-    frames = data_frame.groupby(tKey)
+    frames = data_frame.groupby(t_key)
 
-    times = frames.groups.keys()
+    times = list(frames.groups.keys())
     times.sort()
     return trajectories, frames, times
 
-def checkColumn(keyOptions, data_frame):
-    for key in keyOptions:
+def checkColumn(key_options, data_frame):
+    for key in key_options:
         if key in data_frame.columns:
             return key
-    # print('Failed to find required key (' + keyOptions.join(',') + ')')
-    quit('Failed to find required key (' + ','.join(keyOptions) + ')')
+    quit('Failed to find required key (' + ','.join(key_options) + ')')
 
 if __name__ == '__main__':
     main()
