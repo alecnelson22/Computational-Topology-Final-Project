ab5428cf8ae7c7e981edab7904ab2d253a087f79
Devin Lange <devinscottlange@gmail.com>
2021-04-17 15:06:40 -0600

    add missing /

--- get_dataset_list.py
+++ get_dataset_list.py
@@ -4,4 +4,4 @@ folder_list = ['UNI_CORR_500', 'BI_CORR', 'CROSSING_90', 'CROSSING_120']
 
 for folder in folder_list:
     for path in Path('./data/' + folder).rglob('*.csv'):
-        print('.' + str(path))
+        print('./' + str(path))
