e6a576a8d9be6caf0ff6a5b5189c16f5289e1a2f
Alec <alecnelson52@yahoo.com>
2021-04-23 09:40:59 -0600

    general regression framework; train and predict from persistance fvecs

---
+++ regressors.py
@@ -0,0 +1,71 @@
+import sys, os
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import SGDRegressor
+from sklearn.svm import SVR
+
+
+def load_full_dataset(fname):
+    full_dataset = np.load(fname)
+    X = full_dataset.f.X
+    y_avg = full_dataset.f.y_avg
+    y_med = full_dataset.f.y_med
+    return X, y_avg, y_med
+
+
+def print_stats(model_name, n_train, n_test, R2_y_avg, R2_y_med):
+    print('MODEL: ', model_name)
+    print('Number of train: ', n_train)
+    print('Number of test: ', n_test)
+    print('R2 y_avg: ', R2_y_avg)
+    print('R2_y_med: ', R2_y_med, '\n')
+
+
+def main(fname):
+    LR = ['Linear Regression', LinearRegression()]
+    SGD = ['Stochastic Gradient Descent', SGDRegressor()]
+    SVMR = ['Support Vector Regression', SVR()]
+    models = [LR, SGD, SVMR]
+
+    train_percent = .8  # percentage of examples for training; complement is for testing
+    X, y_avg, y_med = load_full_dataset(fname)
+    n_train = int(len(X) * train_percent)
+    n_test = len(X) - n_train
+
+    for m in models:
+        model_name = m[0]
+        model = m[1]
+
+        train_feats = X[:n_train]
+        train_y_avg = y_avg[:n_train]
+        train_y_med = y_med[:n_train]
+
+        test_feats = X[n_train:]
+        test_y_avg = y_avg[n_train:]
+        test_y_med = y_med[n_train:]
+
+        # fit to y_avg
+        model.fit(train_feats, train_y_avg)
+        preds_y_avg = model.predict(test_feats)
+        score_y_avg = model.score(test_feats, test_y_avg)
+        # fit to y_med
+        model.fit(train_feats, train_y_med)
+        preds_y_med = model.predict(test_feats)
+        score_y_med = model.score(test_feats, test_y_med)
+
+        print_stats(model_name, n_train, n_test, score_y_avg, score_y_med)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        # main(DEFAULT_DATASET)
+        main('persistence_image_feature/full_dataset.npz')
+        exit(0)
+        # quit("Missing filename as command-line argument. e.g. 'python main.py ./data/Simple/two_ortho.csv'")
+    filename = sys.argv[1]
+    if not os.path.exists(filename):
+        quit("Filename '{}' does not exist".format(filename))
+    main(filename)
+    # plot_risk(filename, ids=[1967, 1934, 1871])  # todo provide ids as command line args
